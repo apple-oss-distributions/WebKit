c1f46044c1b6f2746c8f2286cdbfdab358ca8987
Apple OSS Distributions <91980991+AppleOSSDistributions@users.noreply.github.com>
2022-11-28 22:51:29 +0000

    WebKit-7614.3.7.1.5
    Imported from WebKit-7614.3.7.1.5.tar.gz

--- Source/WebCore/page/ViewportConfiguration.h
+++ Source/WebCore/page/ViewportConfiguration.h
@@ -129,7 +129,6 @@ public:
     double layoutSizeScaleFactor() const { return m_layoutSizeScaleFactor; }
 
     void setPrefersHorizontalScrollingBelowDesktopViewportWidths(bool value) { m_prefersHorizontalScrollingBelowDesktopViewportWidths = value; }
-    void setCanIgnoreViewportArgumentsToAvoidExcessiveZoom(bool value) { m_canIgnoreViewportArgumentsToAvoidExcessiveZoom = value; }
 
     WEBCORE_EXPORT IntSize layoutSize() const;
     WEBCORE_EXPORT int layoutWidth() const;
@@ -213,7 +212,6 @@ private:
     bool m_forceAlwaysUserScalable;
     bool m_isKnownToLayOutWiderThanViewport { false };
     bool m_prefersHorizontalScrollingBelowDesktopViewportWidths { false };
-    bool m_canIgnoreViewportArgumentsToAvoidExcessiveZoom { false };
 };
 
 WTF::TextStream& operator<<(WTF::TextStream&, const ViewportConfiguration::Parameters&);
--- Source/WebCore/platform/graphics/avfoundation/MediaPlayerPrivateAVFoundation.cpp
+++ Source/WebCore/platform/graphics/avfoundation/MediaPlayerPrivateAVFoundation.cpp
@@ -374,18 +374,8 @@ void MediaPlayerPrivateAVFoundation::setReadyState(MediaPlayer::ReadyState state
     if (state == m_readyState)
         return;
 
-    auto oldState = std::exchange(m_readyState, state);
+    m_readyState = state;
     m_player->readyStateChanged();
-
-    if (oldState >= MediaPlayer::ReadyState::HaveMetadata)
-        return;
-
-    // Many state methods in MediaPlayerPrivateAVFoundation will return defaults
-    // if queried before reaching HaveMetadata. Re-fire their changed events after
-    // the ready state moves beyond HaveMetadata so the correct values are reflected
-    // upwards to clients.
-    if (!m_cachedNaturalSize.isEmpty())
-        m_player->sizeChanged();
 }
 
 void MediaPlayerPrivateAVFoundation::characteristicsChanged()
@@ -587,6 +577,9 @@ void MediaPlayerPrivateAVFoundation::updateStates()
         }
     }
 
+    if (isReadyForVideoSetup() && currentRenderingMode() != preferredRenderingMode())
+        setUpVideoRendering();
+
     if (firstVideoFrameBecomeAvailable) {
         if (m_readyState < MediaPlayer::ReadyState::HaveCurrentData)
             newReadyState = MediaPlayer::ReadyState::HaveCurrentData;
@@ -600,9 +593,6 @@ void MediaPlayerPrivateAVFoundation::updateStates()
 
     setNetworkState(newNetworkState);
     setReadyState(newReadyState);
-
-    if (isReadyForVideoSetup() && currentRenderingMode() != preferredRenderingMode())
-        setUpVideoRendering();
 }
 
 void MediaPlayerPrivateAVFoundation::setPageIsVisible(bool visible)
